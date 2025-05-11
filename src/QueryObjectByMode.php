bd981f25b4e88d8b85ad51d6631c19bb8b2d2bf3
Tomáš Kudělka <thorewi@gmail.com>
2025-05-11 12:52:51 +0200

    Update QueryObjectByMode.php

--- src/QueryObjectByMode.php
+++ src/QueryObjectByMode.php
@@ -4,14 +4,15 @@ namespace ADT\DoctrineComponents;
 
 enum QueryObjectByMode: string
 {
-	case STRICT = 'strict';
+	case AUTO = 'auto';
+	case EQUAL = 'equal';
 	case NOT_EQUAL = 'notEqual';
 	case STARTS_WITH = 'startsWith';
 	case ENDS_WITH = 'endsWith';
 	case CONTAINS = 'contains';
 	case NOT_CONTAINS = 'notContains';
-	case IS_EMPTY = 'isEmpty';
-	case IS_NOT_EMPTY = 'isNotEmpty';
+	case IS_NULL = 'isNull';
+	case IS_NOT_NULL = 'isNotNull';
 	case IN_ARRAY = 'isInArray';
 	case NOT_IN_ARRAY = 'isNotInArray';
 	case GREATER = 'greater';
@@ -21,4 +22,6 @@ enum QueryObjectByMode: string
 	case BETWEEN = 'between';
 	case NOT_BETWEEN = 'notBetween';
 	case MEMBER_OF = 'memberOf';
+	case IS_EMPTY = 'isEmpty';
+	case IS_NOT_EMPTY = 'isNotEmpty';
 }
