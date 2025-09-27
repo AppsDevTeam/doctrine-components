8ed86b11f72d52a8727015a9ed861ef43c913236
Tomáš Kudělka <tomas@appsdevteam.com>
2025-09-27 08:53:19 +0200

    Marks BaseEntity as a MappedSuperclass
    Marks the `BaseEntity` as a Doctrine `MappedSuperclass`.
    
    This change allows other entities to inherit from it
    without `BaseEntity` being managed as a standalone entity
    in the database.

--- src/Entities/BaseEntity.php
+++ src/Entities/BaseEntity.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace ADT\DoctrineComponents\Entities;
 
 use ADT\DoctrineComponents\Entities\Traits\Identifier;
+use Doctrine\ORM\Mapping\MappedSuperclass;
 
+#[MappedSuperclass]
 abstract class BaseEntity implements Entity
 {
 	use Identifier;
